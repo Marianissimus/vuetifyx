5ba66370097c974363d7ac7bcdfa2e2c62a0039d
Marian Vidoe <marianissimus@gmail.com>
2019-07-05 16:30:11 +0300

    more on Menu: filters (also added "type" to menu.json for said filters); started Events; more on Promotions

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,7 +1,21 @@
 <template>
   <v-container grid-list-lg>
+    <v-tabs class="mb-3" fixed-tabs color="transparent" dark slider-color="brown darken-2" show-arrows>
+      <v-tab>
+        <v-btn @click="filter('all')">all</v-btn>
+      </v-tab>
+      <v-tab>
+        <v-btn @click="filter('breakfast')">breakfast</v-btn>
+      </v-tab>
+      <v-tab>
+        <v-btn @click="filter('lunch')">lunch</v-btn>
+      </v-tab>
+      <v-tab>
+        <v-btn @click="filter('dinner')">dinner</v-btn>
+      </v-tab>
+    </v-tabs>
     <v-layout row wrap align-content-space-around>
-      <v-flex v-for="item in items" :key="item.id" xs12 sm6 md4 lg3>
+      <v-flex v-for="item in filteredItems" :key="item.id" xs12 sm6 md4 lg3>
         <v-card elevation="12" ripple height="320">
           <v-img class="white--text shadow headline black text-capitalize" height="200px" src="https://picsum.photos/200">
             <v-card-title>
@@ -29,7 +43,8 @@ import menuItems from '../data/menu.json'
 export default {
   data () {
     return {
-      items: []
+      items: [],
+      filteredItems: []
     }
   },
   created () {
@@ -38,6 +53,10 @@ export default {
   methods: {
     fetchMenu() {
       this.items = menuItems.menu
+      this.filteredItems = this.items
+    },
+    filter(arg){
+      this.filteredItems = (arg === 'all') ? this.items : this.items.filter(el => el.type.includes(arg))
     }
   }
 }
--- src/components/Promotions.vue
+++ src/components/Promotions.vue
@@ -3,43 +3,35 @@
     <v-layout row wrap>
       <v-flex d-flex xs12 sm6 md4>
         <v-card color="purple" dark>
-          <v-card-title primary class="title">Lorem</v-card-title>
-          <v-card-text>{{ lorem }}</v-card-text>
+          <v-card-title primary class="title">Today, {{ today }}</v-card-title>
+          <v-card-text></v-card-text>
         </v-card>
       </v-flex>
       <v-flex d-flex xs12 sm6 md3>
         <v-layout row wrap>
           <v-flex d-flex>
             <v-card color="indigo" dark>
+              <v-card-title primary class="title">Tomorrow</v-card-title>
               <v-card-text>{{ lorem.slice(0, 70) }}</v-card-text>
             </v-card>
           </v-flex>
           <v-flex d-flex>
-            <v-layout row wrap>
-              <v-flex
-                v-for="n in 2"
-                :key="n"
-                d-flex
-                xs12
-              >
-                <v-card
-                  color="red lighten-2"
-                  dark
-                >
-                  <v-card-text>{{ lorem.slice(0, 40) }}</v-card-text>
-                </v-card>
-              </v-flex>
-            </v-layout>
+            <v-card color="orange" dark>
+              <v-card-title primary class="title">Day After Tomorrow</v-card-title>
+              <v-card-text>{{ lorem.slice(0, 70) }}</v-card-text>
+            </v-card>
           </v-flex>
         </v-layout>
       </v-flex>
       <v-flex d-flex xs12 sm6 md2 child-flex>
         <v-card color="green lighten-2" dark>
+          <v-card-title primary class="title">Every Tuesday</v-card-title>
           <v-card-text>{{ lorem.slice(0, 90) }}</v-card-text>
         </v-card>
       </v-flex>
       <v-flex d-flex xs12 sm6 md3>
         <v-card color="blue lighten-2" dark>
+          <v-card-title primary class="title">Group discounts!</v-card-title>
           <v-card-text>{{ lorem.slice(0, 100) }}</v-card-text>
         </v-card>
       </v-flex>
@@ -50,8 +42,12 @@
 <script>
 export default {
   data: () => ({
-    lorem: `Lorem ipsum dolor sit amet, mel at clita quando. Te sit oratio vituperatoribus, nam ad ipsum posidonium mediocritatem, explicari dissentiunt cu mea. Repudiare disputationi vim in, mollis iriure nec cu, alienum argumentum ius ad. Pri eu justo aeque torquatos.`
-  })
+    lorem: `Lorem ipsum dolor sit amet, mel at clita quando. Te sit oratio vituperatoribus, nam ad ipsum posidonium mediocritatem, explicari dissentiunt cu mea. Repudiare disputationi vim in, mollis iriure nec cu, alienum argumentum ius ad. Pri eu justo aeque torquatos.`,
+    today: new Date().toString().slice(0, 15),
+  }),
+  methods: {
+
+  }
 }
 </script>
 
